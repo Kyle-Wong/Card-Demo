f2b6e5e40ae45836e05393d2b6472037141cb97c
Kyle Wong <kylewonga@gmail.com>
2021-01-04 22:28:08 -0800

    Added cards and deck class

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Card
+{
+
+  // Use this for initialization
+  public static readonly string[] suitTypes = { "Hearts", "Diamonds", "Clubs", "Spades" };
+  public static readonly string[] valueNames = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+  private int suit;
+  private int value;
+
+  public Card(int suit, int value)
+  {
+    this.suit = suit;
+    this.value = value;
+  }
+  public string SuitType()
+  {
+    return suitTypes[suit];
+  }
+  public string ValueName()
+  {
+    return valueNames[value];
+  }
+}
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+
+public class Deck
+{
+
+  // Use this for initialization
+  private Stack<Card> cardStack;
+  private Random rng;
+  public Deck(int numDecks)
+  {
+    this.cardStack = this.GenerateDeck(numDecks);
+  }
+  public Deck(int numDecks, int seed)
+  {
+    rng = new Random(seed);
+    this.cardStack = this.GenerateDeck(numDecks);
+  }
+  // Update is called once per frame
+  public Card DrawCard()
+  {
+    return cardStack.Pop();
+  }
+
+  private Stack<Card> GenerateDeck(int numDecks)
+  {
+    Stack<Card> newDeck = new Stack<Card>();
+    for (int i = 0; i < numDecks; i++)
+    {
+      for (int value = 0; value < 13; value++)
+      {
+        for (int suit = 0; suit < 4; suit++)
+        {
+          newDeck.Push(new Card(suit, value));
+        }
+      }
+    }
+
+    return this.Shuffle(newDeck);
+  }
+  public Stack<Card> Shuffle(Stack<Card> deck)
+  {
+    List<Card> shuffleList = new List<Card>(deck);
+    for (int i = 0; i < shuffleList.Count; i++)
+    {
+      int randIndex = rng.Next();
+      Card temp = shuffleList[i];
+      shuffleList[i] = shuffleList[randIndex];
+      shuffleList[randIndex] = temp;
+    }
+    return new Stack<Card>(shuffleList);
+  }
+
+
+}
